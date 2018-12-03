bfc9291df101bc92eb84c23c5f16528b117d9f48
rm41001 <45181845+rm41001@users.noreply.github.com>
2018-12-03 14:08:06 -0600

    Add files via upload

---
+++ quiz.php
@@ -0,0 +1,18 @@
+<!-- Urvi Awasthi and Red Maier; November 29th, 2018; Arrays Exercise -->
+<!DOCTYPE html> 
+<html>
+	<head>
+		<!--styling the title, and adding an external style sheet -->
+		<title> Quiz </title>
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+	</head>
+	
+	<body>
+		<h1> Here is the quiz, good luck!  </h1>
+	
+		<form action = "results.html" method = "POST">
+				<input type = "submit" value = "submit"> 	<!-- Button to go back to log in -->
+		</form>
+
+	</body>
+</html>
---
+++ results.php
@@ -0,0 +1,19 @@
+<!-- Urvi Awasthi and Red Maier; November 29th, 2018; Arrays Exercise -->
+<!DOCTYPE html> 
+<html>
+	<head>
+		<!--styling the title, and adding an external style sheet -->
+		<title> Results </title>
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+	</head>
+	
+	<body>
+		<h1> Thank you for finishing the quiz! Here are your results: </h1>
+	
+		<!--Put results of quiz here later -->
+		<form action = "logout.html" method = "POST">
+				<input type = "submit" value = "Logout"> 	<!-- Button to go back to log in -->
+		</form>
+
+	</body>
+</html>
