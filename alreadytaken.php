424323350dc84d39020479ee06aa4287f5af09ec
rm41001 <45181845+rm41001@users.noreply.github.com>
2018-12-06 12:43:59 -0600

    Update alreadytaken.php

--- alreadytaken.php
+++ alreadytaken.php
@@ -1,9 +1,9 @@
-<!-- Urvi Awasthi and Red Maier; November 29th, 2018; Arrays Exercise -->
+<!-- Urvi Awasthi and Red Maier; November 29th, 2018; Quarter Project -->
 <!DOCTYPE html> 
 <html>
 	<head>
 		<!--styling the title, and adding an external style sheet -->
-		<title> Already Taken </title>
+		<title> Already Taken </title> <!-- The purpose of this is to display a page where the user has already taken the quiz and is not allowed to take it again -->
 		<link rel = "stylesheet" type = "text/css" href = "style.css">
 	</head>
 	
