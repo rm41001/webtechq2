0040f23bb7eb3f6461fb8774871ed0b0a77a6774
rm41001 <45181845+rm41001@users.noreply.github.com>
2018-12-06 13:58:11 -0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -6,6 +6,9 @@
 			include "config.php";
 			$un = $_POST['username'];
 			$pw = $_POST['password'];
+			$cookie_name = $_POST['username'];
+			$cookie_value = "John Doe";
+			setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 			mysqli_select_db($conn, 'login');
 			$sql="SELECT * FROM register WHERE username='$un'";
 			$result = mysqli_query($conn, $sql);
